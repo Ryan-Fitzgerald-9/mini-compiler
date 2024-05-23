6a19f6535188e0ad7c1ec555279dbbea1bb448fc
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2024-05-23 16:23:49 -0400

    parse.py initialized and functions updated

--- parse.py
+++ parse.py
@@ -4,23 +4,31 @@ from lex import *
 # Parser object keeps track of current token and checks if the code matches the grammar
 class Parser:
     def __init__(self, lexer):
-        pass
+        self.lexer = lexer
+
+        self.curToken = None
+        self.peekToken = None
+        self.nextToken()
+        self.nextToken() # Called twice to initialize current and peek
 
     # Return true if current token matches
     def checkToken(self, kind):
-        pass
+        return kind == self.curToken.kind
 
     # Return true if the next token matches
     def checkPeek(self, kind):
-        pass
+        return kind == self.peekToken.kind
 
     # Try to match current token
     def match(self, kind):
-        pass
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
+        self.nextToken()
 
     # Advances the current token
     def nextToken(self):
-        pass
+        self.curToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
 
     def abort(self, message):
         sys.exit("Error. " + message)
